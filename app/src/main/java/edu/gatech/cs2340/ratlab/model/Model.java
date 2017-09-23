67c48fd78c056e7219408a002ead7ef73cdf4b37
Timothy Gieseking <tgieseking2.71@gmail.com>
2017-09-23 19:25:16 -0400

    Added a getModel() method to get the model from other classes

--- app/src/main/java/edu/gatech/cs2340/ratlab/model/Model.java
+++ app/src/main/java/edu/gatech/cs2340/ratlab/model/Model.java
@@ -1,6 +1,10 @@
 package edu.gatech.cs2340.ratlab.model;
 
 public class Model {
+
+    private static final Model instance = new Model();
+    public static Model getInstance() { return instance; }
+
     boolean loggedIn;
 
     //I didn't know where to put the dummy user but I guess here works for now
