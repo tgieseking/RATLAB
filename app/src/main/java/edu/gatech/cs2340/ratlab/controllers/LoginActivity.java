55295f0ff689a3a93639b7de4b59e513554b2e87
Bryan Liang <bryanliang7@gmail.com>
2017-09-25 14:21:23 -0400

    -Create registration activity & layout (M5)

--- app/src/main/java/edu/gatech/cs2340/ratlab/controllers/LoginActivity.java
+++ app/src/main/java/edu/gatech/cs2340/ratlab/controllers/LoginActivity.java
@@ -296,7 +296,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     /**
-     * Represents an asynchronous login/registration task used to authenticate
+     * Represents an asynchronous login/RegistrationActivity task used to authenticate
      * the user.
      */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
