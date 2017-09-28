765e0bcb9a9a5c1f423ed9a4ebcfbd65f9fde778
Timothy Gieseking <tgieseking2.71@gmail.com>
2017-09-28 17:31:23 -0400

    Added text boxes and button to registration activity

--- app/src/main/java/edu/gatech/cs2340/ratlab/controllers/RegistrationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/ratlab/controllers/RegistrationActivity.java
@@ -3,13 +3,18 @@ package edu.gatech.cs2340.ratlab.controllers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import edu.gatech.cs2340.ratlab.R;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 }
