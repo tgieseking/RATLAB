f550567780b5794763c35744d149604d712455d0
Timothy Gieseking <tgieseking2.71@gmail.com>
2017-10-04 20:13:12 -0400

    CSV Reading
    The rat_sightings.csv table is now included in the resources/raw. This
    file is read upon startup and rat reports are added to the
    ratSightingList.

--- app/src/main/java/edu/gatech/cs2340/ratlab/model/Model.java
+++ app/src/main/java/edu/gatech/cs2340/ratlab/model/Model.java
@@ -1,6 +1,14 @@
 package edu.gatech.cs2340.ratlab.model;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import edu.gatech.cs2340.ratlab.R;
 
 public class Model {
 
@@ -10,5 +18,26 @@ public class Model {
     // Stores information on the currently logged in user
     private User currentUser;
 
+    // The list of all rat sightings
     private List<RatSighting> ratSightings;
+
+    Model() {
+        ratSightings = new ArrayList<>();
+    }
+
+    public void readHistoricalData(Context context) {
+        InputStream historicalDataStream = context.getResources().openRawResource(R.raw.rat_sightings);
+        Scanner historicalDataScanner = new Scanner(historicalDataStream);
+        historicalDataScanner.nextLine(); // Don't read the header
+
+        RatSighting currentSighting;
+        String nextLine;
+        while (historicalDataScanner.hasNextLine()) {
+            nextLine = historicalDataScanner.nextLine();
+            currentSighting = RatSighting.createRatSightingFromCsvLine(nextLine);
+            if (currentSighting != null) {
+                ratSightings.add(currentSighting);
+            }
+        }
+    }
 }
--- app/src/main/java/edu/gatech/cs2340/ratlab/model/RatSighting.java
+++ app/src/main/java/edu/gatech/cs2340/ratlab/model/RatSighting.java
@@ -1,10 +1,12 @@
 package edu.gatech.cs2340.ratlab.model;
 
+import android.util.Log;
+
 import java.util.Date;
 
 public class RatSighting {
     private String key;
-    private Date createdDate;
+    private String createdDate;  // This maybe should be a java.util.Date
     private String locationType;
     private String address;
     private String zipCode;
@@ -12,4 +14,45 @@ public class RatSighting {
     private Borough borough;
     private double latitude;
     private double longitude;
+
+    RatSighting(String key, String createdDate, String locationType, String address, String zipCode,
+                String city, Borough borough, double latitude, double longitude) {
+        this.key = key;
+        this.createdDate = createdDate;
+        this.locationType = locationType;
+        this.address = address;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.borough = borough;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    static RatSighting createRatSightingFromCsvLine(String csvLine) {
+        String[] splitLine = csvLine.split(",");
+        if (splitLine.length == 9) {
+            // Expected length of line
+            Borough borough = null;
+            double latitude = 0;
+            double longitude = 0;
+            try {
+                borough = Borough.valueOf(splitLine[6]);
+                latitude = Double.parseDouble(splitLine[7]);
+                longitude = Double.parseDouble(splitLine[8]);
+            } catch (Exception e) {
+                Log.d("historical_data", "Row with id " + splitLine[0] + " could not be parsed");
+            }
+            return new RatSighting(splitLine[0], splitLine[1], splitLine[2], splitLine[4],
+                    splitLine[3], splitLine[5], borough, latitude, longitude);
+        } else if (splitLine.length > 0){
+            Log.d("historical_data", "Row with id " + splitLine[0] + " has too few rows");
+        } else {
+            Log.d("historical_data", "Encountered empty row");
+        }
+        return null;
+    }
+
+    public String toString() {
+        return key + createdDate + locationType + address + zipCode + city + borough + latitude + longitude;
+    }
 }
