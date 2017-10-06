0d9198461538d7e193dc6c7e8e9630d83d374994
Timothy Gieseking <tgieseking2.71@gmail.com>
2017-10-05 23:09:38 -0400

    Rat sightings in model syncs with database

--- app/src/main/java/edu/gatech/cs2340/ratlab/model/Model.java
+++ app/src/main/java/edu/gatech/cs2340/ratlab/model/Model.java
@@ -2,6 +2,15 @@ package edu.gatech.cs2340.ratlab.model;
 
 import android.content.Context;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -13,6 +22,7 @@ import java.util.Scanner;
 import java.util.Set;
 
 import edu.gatech.cs2340.ratlab.R;
+import edu.gatech.cs2340.ratlab.controllers.MainActivity;
 
 public class Model {
 
@@ -25,8 +35,58 @@ public class Model {
     // The list of all rat sightings
     private Map<String,RatSighting> ratSightings;
 
+    // Database variables for syncing the rat sighting list with firebase
+    private DatabaseReference sightingsReference;
+    private ChildEventListener sightingsListener;
+
     Model() {
         ratSightings = new HashMap<>();
+        sightingsReference = FirebaseDatabase.getInstance().getReference().child("sightings");
+
+        sightingsListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                RatSighting sighting = createRatSighting(dataSnapshot);
+                ratSightings.put(sighting.getKey(), sighting);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                RatSighting sighting = createRatSighting(dataSnapshot);
+                ratSightings.put(sighting.getKey(), sighting);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                ratSightings.remove(dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+                RatSighting sighting = createRatSighting(dataSnapshot);
+                ratSightings.put(sighting.getKey(), sighting);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("sighting_database", databaseError.toString());
+            }
+
+            private RatSighting createRatSighting(DataSnapshot dataSnapshot) {
+                String key = dataSnapshot.getKey();
+                String createdDate = (String) dataSnapshot.child("createdDate").getValue();
+                String locationType = (String) dataSnapshot.child("locationType").getValue();
+                String address = (String) dataSnapshot.child("address").getValue();
+                String zipCode = (String) dataSnapshot.child("zipCode").getValue();
+                String city = (String) dataSnapshot.child("city").getValue();
+                Borough borough = Borough.valueOf((String) dataSnapshot.child("borough").getValue());
+                double latitude = (double) dataSnapshot.child("latitude").getValue();
+                double longitude = (double) dataSnapshot.child("longitude").getValue();
+                return new RatSighting(key, createdDate, locationType, address, zipCode, city,
+                        borough, latitude, longitude);
+            }
+        };
+        sightingsReference.addChildEventListener(sightingsListener);
     }
 
     public void readHistoricalData(Context context) {
