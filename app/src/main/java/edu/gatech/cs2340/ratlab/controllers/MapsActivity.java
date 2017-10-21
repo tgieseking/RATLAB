472d2f612e76e96bc78891b83c79a2bc430227cc
Timothy Gieseking <tgieseking2.71@gmail.com>
2017-10-21 17:37:43 -0400

    Moved MapsActivity to Controllers folder

--- app/src/main/java/edu/gatech/cs2340/ratlab/controllers/MapsActivity.java
+++ app/src/main/java/edu/gatech/cs2340/ratlab/controllers/MapsActivity.java
@@ -1,4 +1,4 @@
-package edu.gatech.cs2340.ratlab;
+package edu.gatech.cs2340.ratlab.controllers;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -9,6 +9,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import edu.gatech.cs2340.ratlab.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
